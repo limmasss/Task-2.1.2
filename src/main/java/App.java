cf2767f7a36edec0fb3f1c9f65cb7fd64c93d0a9
Dmitry Onoprienko <limmasss@mail.ru>
2021-08-23 21:58:05 +0300

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1 == bean2);
+        Cat cat1 = applicationContext.getBean(Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
+        System.out.println(cat1 == cat2);
+        System.out.println(bean1.getMessage());
     }
 }
